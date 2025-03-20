e33108ee58b98504c60f3fc38e6a3dad90ada0a4
xilanhua <51257868+xilanhu@users.noreply.github.com>
2025-03-20 22:46:35 +0800

    Update system.py

--- kiauh/procedures/system.py
+++ kiauh/procedures/system.py
@@ -25,46 +25,45 @@ def change_system_hostname() -> None:
     Logger.print_dialog(
         DialogType.CUSTOM,
         [
-            "Changing the hostname of this system allows you to access an installed "
-            "webinterface by simply typing the hostname like this in the browser:",
+            "修改系统主机名后,"
+            "您可以通过以下格式在浏览器中访问已安装的Web界面:",
             "\n\n",
             "http://<hostname>.local",
             "\n\n",
-            "Example: If you set your hostname to 'my-printer', you can access an "
-            "installed webinterface by typing 'http://my-printer.local' in the "
-            "browser.",
+            "示例：如果将主机名设置为'my-printer',可通过访问 'http://my-printer.local'",
+            "进入控制界面.",
         ],
-        custom_title="CHANGE SYSTEM HOSTNAME",
+        custom_title="修改系统主机名",
     )
-    if not get_confirm("Do you want to change the hostname?", default_choice=False):
+    if not get_confirm("您要更改主机名吗?", default_choice=False):
         return
 
     Logger.print_dialog(
         DialogType.CUSTOM,
         [
-            "Allowed characters: a-z, 0-9 and '-'",
-            "The name must not contain the following:",
+            "允许使用的字符: 小写字母 a-z、数字 0-9 和连字符 '-'",
+            "命名必须遵守以下规则:",
             "\n\n",
-            "● Any special characters",
-            "● No leading or trailing '-'",
+            "● 不能包含特殊字符",
+            "● 开头或结尾不能使用连字符 '-'",
         ],
     )
     hostname = get_string_input(
-        "Enter the new hostname",
+        "输入新的主机名",
         regex=r"^[a-z0-9]+([a-z0-9-]*[a-z0-9])?$",
     )
-    if not get_confirm(f"Change the hostname to '{hostname}'?", default_choice=False):
-        Logger.print_info("Aborting hostname change ...")
+    if not get_confirm(f"确定将主机名更改为 '{hostname}'?", default_choice=False):
+        Logger.print_info("已取消更改主机名 ...")
         return
 
     try:
-        Logger.print_status("Changing hostname ...")
+        Logger.print_status("正在更改主机名 ...")
 
-        Logger.print_status("Checking for dependencies ...")
+        Logger.print_status("检查依赖项 ...")
         check_install_dependencies({"avahi-daemon"}, include_global=False)
 
         # create or backup hosts file
-        Logger.print_status("Creating backup of hosts file ...")
+        Logger.print_status("正在备份 hosts 文件 ...")
         hosts_file = Path("/etc/hosts")
         if not check_file_exist(hosts_file, True):
             cmd = ["sudo", "touch", hosts_file.as_posix()]
@@ -83,21 +82,21 @@ def change_system_hostname() -> None:
         Logger.print_ok()
 
         # call hostnamectl set-hostname <hostname>
-        Logger.print_status(f"Setting hostname to '{hostname}' ...")
+        Logger.print_status(f"正在设置主机名为 '{hostname}' ...")
         cmd = ["sudo", "hostnamectl", "set-hostname", hostname]
         run(cmd, stderr=PIPE, check=True)
         Logger.print_ok()
 
         # add hostname to hosts file at the end of the file
-        Logger.print_status("Writing new hostname to /etc/hosts ...")
+        Logger.print_status("正在写入 /etc/hosts ...")
         stdin = f"127.0.0.1       {hostname}\n"
         cmd = ["sudo", "tee", "-a", hosts_file.as_posix()]
         run(cmd, input=stdin.encode(), stderr=PIPE, stdout=PIPE, check=True)
         Logger.print_ok()
 
-        Logger.print_ok("New hostname successfully configured!")
-        Logger.print_ok("Remember to reboot for the changes to take effect!\n")
+        Logger.print_ok("新主机名配置成功!")
+        Logger.print_ok("请重启系统以使更改生效!\n")
 
     except CalledProcessError as e:
-        Logger.print_error(f"Error during change hostname procedure: {e}")
+        Logger.print_error(f"更改主机名过程中发生错误: {e}")
         return
