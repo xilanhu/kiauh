6e8eef2b45f841552edc79b558918d4456576380
xilanhua <51257868+xilanhu@users.noreply.github.com>
2025-03-20 22:20:28 +0800

    Update octoeverywhere_extension.py

--- kiauh/extensions/octoeverywhere/octoeverywhere_extension.py
+++ kiauh/extensions/octoeverywhere/octoeverywhere_extension.py
@@ -44,7 +44,7 @@ from utils.sys_utils import (
 # noinspection PyMethodMayBeStatic
 class OctoeverywhereExtension(BaseExtension):
     def install_extension(self, **kwargs) -> None:
-        Logger.print_status("Installing OctoEverywhere for Klipper ...")
+        Logger.print_status("正在为 Klipper 安装 OctoEverywhere ...")
 
         # check if moonraker is installed. if not, notify the user and exit
         if not moonraker_exists():
@@ -56,16 +56,16 @@ class OctoeverywhereExtension(BaseExtension):
             Logger.print_dialog(
                 DialogType.INFO,
                 [
-                    "OctoEverywhere is already installed!",
-                    "It is safe to run the installer again to link your "
-                    "printer or repair any issues.",
+                    "OctoEverywhere 已安装成功!",
+                    "可以安全地再次运行安装程序， "
+                    "打印或处理其他事情.",
                 ],
             )
             if not get_confirm("Re-run OctoEverywhere installation?"):
-                Logger.print_info("Exiting OctoEverywhere for Klipper installation ...")
+                Logger.print_info("退出 OctoEverywhere 的安装程序 ...")
                 return
             else:
-                Logger.print_status("Re-Installing OctoEverywhere for Klipper ...")
+                Logger.print_status("重新安装 OctoEverywhere ...")
                 force_clone = True
 
         mr_instances: List[Moonraker] = get_instances(Moonraker)
@@ -75,19 +75,19 @@ class OctoeverywhereExtension(BaseExtension):
             Logger.print_dialog(
                 DialogType.INFO,
                 [
-                    "The following Moonraker instances were found:",
+                    发现以下 Moonraker 服务:",
                     *mr_names,
                     "\n\n",
-                    "The setup will apply the same names to OctoEverywhere!",
+                    "安装程序将为 OctoEverywhere 应用相同的名称!",
                 ],
             )
 
         if not get_confirm(
-            "Continue OctoEverywhere for Klipper installation?",
+            "是否继续为 Klipper 安装 OctoEverywhere?",
             default_choice=True,
             allow_go_back=True,
         ):
-            Logger.print_info("Exiting OctoEverywhere for Klipper installation ...")
+            Logger.print_info("正在退出 OctoEverywhere 的安装程序 ...")
             return
 
         try:
@@ -101,30 +101,30 @@ class OctoeverywhereExtension(BaseExtension):
 
             Logger.print_dialog(
                 DialogType.SUCCESS,
-                ["OctoEverywhere for Klipper successfully installed!"],
+                ["OctoEverywhere 安装成功!"],
                 center_content=True,
             )
 
         except Exception as e:
             Logger.print_error(
-                f"Error during OctoEverywhere for Klipper installation:\n{e}"
+                f"安装 OctoEverywhere 时发生错误:\n{e}"
             )
 
     def update_extension(self, **kwargs) -> None:
-        Logger.print_status("Updating OctoEverywhere for Klipper ...")
+        Logger.print_status("正在更新 OctoEverywhere for Klipper...")
         try:
             Octoeverywhere.update()
             Logger.print_dialog(
                 DialogType.SUCCESS,
-                ["OctoEverywhere for Klipper successfully updated!"],
+                ["OctoEverywhere 更新成功!"],
                 center_content=True,
             )
 
         except Exception as e:
-            Logger.print_error(f"Error during OctoEverywhere for Klipper update:\n{e}")
+            Logger.print_error(f"更新 OctoEverywhere 时发生错误:\n{e}")
 
     def remove_extension(self, **kwargs) -> None:
-        Logger.print_status("Removing OctoEverywhere for Klipper ...")
+        Logger.print_status("正在卸载 OctoEverywhere for Klipper ...")
 
         mr_instances: List[Moonraker] = get_instances(Moonraker)
         ob_instances: List[Octoeverywhere] = get_instances(Octoeverywhere)
@@ -137,12 +137,12 @@ class OctoeverywhereExtension(BaseExtension):
             run_remove_routines(OE_INSTALLER_LOG_FILE)
             Logger.print_dialog(
                 DialogType.SUCCESS,
-                ["OctoEverywhere for Klipper successfully removed!"],
+                ["OctoEverywhere for Klipper 卸载成功!"],
                 center_content=True,
             )
 
         except Exception as e:
-            Logger.print_error(f"Error during OctoEverywhere for Klipper removal:\n{e}")
+            Logger.print_error(f"卸载 OctoEverywhere 时发生错误:\n{e}")
 
     def _install_oe_dependencies(self) -> None:
         oe_deps = []
@@ -153,7 +153,7 @@ class OctoeverywhereExtension(BaseExtension):
             oe_deps = parse_packages_from_file(OE_INSTALL_SCRIPT)
 
         if not oe_deps:
-            raise ValueError("Error reading OctoEverywhere dependencies!")
+            raise ValueError("读取 OctoEverywhere 依赖项时发生错误!")
 
         check_install_dependencies({*oe_deps})
         install_python_requirements(OE_ENV_DIR, OE_REQ_FILE)
@@ -163,7 +163,7 @@ class OctoeverywhereExtension(BaseExtension):
         instance_list: List[Octoeverywhere],
     ) -> None:
         if not instance_list:
-            Logger.print_info("No OctoEverywhere instances found. Skipped ...")
+            Logger.print_info("未找到 OctoEverywhere 服务. 跳过 ...")
             return
 
         for instance in instance_list:
@@ -173,19 +173,19 @@ class OctoeverywhereExtension(BaseExtension):
             InstanceManager.remove(instance)
 
     def _remove_oe_dir(self) -> None:
-        Logger.print_status("Removing OctoEverywhere for Klipper directory ...")
+        Logger.print_status("正在移除 OctoEverywhere 的文件 ...")
 
         if not OE_DIR.exists():
-            Logger.print_info(f"'{OE_DIR}' does not exist. Skipped ...")
+            Logger.print_info(f"'{OE_DIR}' 不存在，跳过 ...")
             return
 
         run_remove_routines(OE_DIR)
 
     def _remove_oe_env(self) -> None:
-        Logger.print_status("Removing OctoEverywhere for Klipper environment ...")
+        Logger.print_status("正在移除 OctoEverywhere 虚拟环境 ...")
 
         if not OE_ENV_DIR.exists():
-            Logger.print_info(f"'{OE_ENV_DIR}' does not exist. Skipped ...")
+            Logger.print_info(f"'{OE_ENV_DIR}' 不存在，跳过 ...")
             return
 
         run_remove_routines(OE_ENV_DIR)
